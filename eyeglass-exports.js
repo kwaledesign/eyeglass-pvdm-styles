50699d78b8bc2bc80782186ac837e518abace8bd
Sam Ashley <sam@kwaledesign.com>
2015-06-10 14:53:17 -0600

    more tweaks to try and get imports working

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -4,11 +4,7 @@ var path = require('path');
 
 module.exports = function(eyeglass, sass) {
   return {
-    sassDir: path.join(__dirname, 'sass'),
-    functions: {
-      'hello($name: \'World\')': function(name, done) {
-        done(sass.types.String('Hello, ' + name.getValue()));
-      }
-    }
+    sassDir: "sass",
+    functions: {}
   };
 };
